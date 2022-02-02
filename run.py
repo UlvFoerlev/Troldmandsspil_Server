999b5c4be2834c98b0e4817b7e41fc1006bae2d4
UlvFoerlev <thesilentparadox@gmail.com>
2022-02-02 15:47:11 +0100

    Refactor

--- run.py
+++ run.py
@@ -3,19 +3,4 @@ from server.core.server import ServerCore
 if __name__ == "__main__":
     core = ServerCore()
 
-    with core() as s:
-        while True:
-
-            bytesAddressPair = s.recv(1024)
-            if bytesAddressPair:
-                print(bytesAddressPair)
-
-            message = bytesAddressPair[0]
-
-            address = bytesAddressPair[1]
-
-            clientMsg = "Message from Client:{}".format(message)
-            clientIP = "Client IP Address:{}".format(address)
-
-            print(clientMsg)
-            print(clientIP)
+    core()
--- server/core/server.py
+++ server/core/server.py
@@ -1,21 +1,46 @@
 from server.core.helpers import getNetworkIp
 import socket
 
+import logging
+
+log = logging.getLogger(__name__)
+
 
 class ServerCore:
     def __init__(self, port: int = 8910):
         self.server_ip = "localhost"
         self.port = port
+        self.socket = None
 
-    def __call__(self):
-        print(f"Server started on {self.server_ip}:{self.port}")
-        print("Waiting for incoming connections.")
+    def open_socket(self):
+        log.info(f"Opening socket on {self.server_ip}:{self.port}")
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         self.socket.bind((self.server_ip, self.port))
 
         return self.socket
+
+    def __call__(self):
+        if not self.socket:
+            self.open_socket()
+
+        log.info(f"Start listening so socket on {self.server_ip}:{self.port}")
+        with self.socket as socket:
+            while True:
+                bytesAddressPair = socket.recv(1024)
+                if bytesAddressPair:
+                    print(bytesAddressPair)
+
+                message = bytesAddressPair[0]
+
+                address = bytesAddressPair[1]
+
+                clientMsg = "Message from Client:{}".format(message)
+                clientIP = "Client IP Address:{}".format(address)
+
+                print(clientMsg)
+                print(clientIP)
 
     def exit():
         pass
